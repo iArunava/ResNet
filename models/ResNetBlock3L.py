4d9aafc416979663462d84983deda88b28c0812f
Arunava <learningdroidarunava@gmail.com>
2019-02-16 18:29:46 +0530

    Update ResNetBlock3L.py

--- models/ResNetBlock3L.py
+++ models/ResNetBlock3L.py
@@ -1,5 +1,5 @@
 class ResNetBlock3L(nn.Module):
-  def __init__(self, ic_conv1, oc_conv1, downsample):
+  def __init__(self, ic_conv1, oc_conv1, downsample, stride=2):
     '''
     '''
     
@@ -8,15 +8,16 @@ class ResNetBlock3L(nn.Module):
     assert (downsample == True or downsample == False)
     self.downsample = downsample
     
-    stride = 1
     if self.downsample:
-        stride = 2
-        
+      stride = stride
+    else:
+      stride = 1
+    
     self.conv1 = BNConv(in_channels=ic_conv1,
-                   out_channels=oc_conv1,
-                   kernel_size=1,
-                   padding=1,
-                   stride=1)
+                         out_channels=oc_conv1,
+                         kernel_size=1,
+                         padding=0,
+                         stride=1)
     
     self.conv2 = BNConv(in_channels=oc_conv1,
                         out_channels=oc_conv1,
@@ -25,9 +26,9 @@ class ResNetBlock3L(nn.Module):
                         stride=stride)
     
     self.conv3 = BNConv(in_channels=oc_conv1,
-                        out_channel=oc_conv1*4,
+                        out_channels=oc_conv1*4,
                         kernel_size=1,
-                        padding=1,
+                        padding=0,
                         stride=1)
     
     self.convs = BNConv(in_channels=ic_conv1,
@@ -50,5 +51,5 @@ class ResNetBlock3L(nn.Module):
     
     x = xm + xs
     x = self.relu(x)
-    
+
     return x
